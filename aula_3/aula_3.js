3ce67f7dde2a739a02de774fe0d606f004c9175d
Gustavo Aparecido da Silva <guch4n@hotmail.com>
2026-07-03 22:01:13 -0300

    Aula 3

--- aula_3/aula_3.js
+++ aula_3/aula_3.js
@@ -14,6 +14,7 @@ const carros = [
     placa: "ABC1234",
     marca: "VW",
     modelo: "Fusca",
+    cor: "Azul",
     tetoSolar: false,
     chassi: "AA12345678"
     },
@@ -21,11 +22,37 @@ const carros = [
     placa: "ads1245",
     marca: "Chevrolet",
     modelo: "Chevette",
-    tetoSolar: false,
+    cor: "Vermelho",
+    tetoSolar: true,
     chassi: "AA133345678"
     }
 ];
 
 // alert(`O carro é : ${carro.marca} ${carro.modelo} - ${carro.placa}`);
-console.log(carros[0].placa);
-console.log(carros[1].placa);
+// console.log(carros[0].placa);
+// console.log(carros[1].placa);
+
+// for(const carro of carros) {
+//     console.log(carro.modelo);
+// }
+
+function popularTabela(){
+    const tbody = document.querySelector("#tabela_carros tbody");
+    let html = "";
+    for (const carro of carros){
+        html += `
+            <tr>
+                <td>${carro.marca}</td>
+                <td>${carro.modelo}</td>
+                <td>${carro.cor}</td>
+                <td>${carro.placa}</td>
+                <td>${carro.chassi}</td>
+                <td>${carro.tetoSolar ? "Sim" : "Não"}</td>
+            </tr>
+        `;
+    }
+
+    tbody.innerHTML = html;
+}
+
+popularTabela();
