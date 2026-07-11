8c738f55d14cd94c07c952b2caf1306e021152c2
Gustavo Aparecido da Silva <guch4n@hotmail.com>
2026-07-10 21:59:56 -0300

    Aula API

---
+++ aula_4/aula_4.js
@@ -0,0 +1,13 @@
+async function buscarCep() {
+ const cepDigitado = document.querySelector("#cep").value;
+ const url = `https://viacep.com.br/ws/${cepDigitado}/json/`;
+
+ const resposta = await fetch(url);
+ const dados = await resposta.json();
+
+ console.log(dados);
+}
+
+function popularCampos(dados) {
+
+}
