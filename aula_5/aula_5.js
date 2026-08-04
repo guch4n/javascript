a8a2c1da1062ce70498f36d6de810baedbb90c13
Gustavo Aparecido da Silva <guch4n@hotmail.com>
2026-08-03 21:51:05 -0300

    Aula final Javascript, CRUD

--- aula_5/aula_5.js
+++ aula_5/aula_5.js
@@ -25,8 +25,8 @@ function popularTabela(produtos) {
             <button class="btn btn-danger" onclick="apagarProduto(${produto.id})">
                 Remover
             </button>
-            <button class="btn btn-primary">
-                Atualizar
+            <button class="btn btn-primary" onclick="editarProduto(${produto.id})">
+                Editar
             </button>
             </td>
         </tr>
@@ -60,12 +60,61 @@ async function apagarProduto(id) {
     }
 }
 
-async function criarProduto() {
-    const produto = { //pega os dados dos campos nome, preco e quantidade
+function modalNovoProduto() {
+    limparFormulario();
+    abrirModal();
+}
+
+async function editarProduto(id) {
+    const url = `${API_URL}/${id}`;
+
+    try { //Sempre que for dados externos é obrigatorio chamar um trycatch
+        const resposta = await fetch(url); //puxa os dados externos
+        const produto = await resposta.json(); //converte para um arquivo javascript
+        popularFormulario(produto);
+        abrirModal();
+    } catch (error) {
+        alert("Não foi possivel editar este produto.");
+    }
+}
+
+function salvarProduto(){
+    const id = Number(document.querySelector("#id").value) || 0;
+    const nome = document.querySelector("#nome").value;
+    const preco = document.querySelector("#preco").value;
+    const quantidade = document.querySelector("#quantidade").value;
+
+    // Não permitir campos vazios - validação
+    if(nome == "" || preco == "" || quantidade == ""
+    ) {
+        alert("Todos os campos são obrigatórios.")
+        return;
+    }
+
+    // Não permitir valores não numéricos preco e quantidade - validação
+    if (!Number(preco) || !Number(quantidade)) {
+        alert("Campo preço e quantidade devem ser numéricos");
+        return;
+    }
+
+    if(id) {
+        atualizarProduto(id);
+        return;
+    }
+
+    criarProduto();
+}
+
+function criarObjetoProduto() {
+    return { //pega os dados dos campos nome, preco e quantidade
         nome: document.querySelector("#nome").value,
         preco: document.querySelector("#preco").value,
         quantidade: document.querySelector("#quantidade").value
     }
+}
+
+async function criarProduto() {
+    const produto = criarObjetoProduto();
 
     try {
         await fetch(API_URL, { //envia para a API, padrão para metodos POST
@@ -85,20 +134,50 @@ async function criarProduto() {
 
 }
 
-function limparFormulario(){
-    document.querySelector("#nome").value = "",
-    document.querySelector("#preco").value = "",
-    document.querySelector("#quantidade").value = ""
+async function atualizarProduto(id) {   
+    const produto = criarObjetoProduto();
+    const url = `${API_URL}/${id}`;
+
+    try {
+        await fetch(url, { //envia para a API, padrão para metodos POST
+            method: "PUT", //Verbo utilizado para atualizar
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(produto)
+        });
+        limparFormulario();
+        fecharModal();
+        buscarProdutos();
+    } catch (error) {
+        alert("Não foi possivel editar o produto.");
+    }
 }
 
+function popularFormulario(produto) {
+    document.querySelector("#id").value = produto.id;
+    document.querySelector("#nome").value = produto.nome;
+    document.querySelector("#preco").value = produto.preco;
+    document.querySelector("#quantidade").value = produto.quantidade;
+}
+function limparFormulario(){
+    document.querySelector("#id").value = "";
+    document.querySelector("#nome").value = "";
+    document.querySelector("#preco").value = "";
+    document.querySelector("#quantidade").value = "";
+}
+
+function abrirModal(){ //
+    const modalHtml = document.querySelector("#modalProduto");
+    const modal = bootstrap.Modal.getOrCreateInstance(modalHtml);
+    modal.show();
+}
 function fecharModal(){ //obrigatorio fazer essa solicitação toda vez que for abrir e fechar um modal
     const modalHtml = document.querySelector("#modalProduto");
     const modal = bootstrap.Modal.getOrCreateInstance(modalHtml);
     modal.hide();
 }
 
-async function atualizarProduto(id) {
-    
-}
+
 
 buscarProdutos();
